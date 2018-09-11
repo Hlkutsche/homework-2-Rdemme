9986adf70a98c7310d2958282440fe12c8026601
Halle Kutsche <hlkutsche@ksu.edu>
2018-09-10 20:48:02 -0500

    Added Kelvin Conversion

--- temperature.sh
+++ temperature.sh
@@ -1,5 +1,6 @@
 echo -n "please enter a temperature in Fahrenheit -->"
 read fahren
 
-echo "scale=2;($fahren-32)*(5/9)" |bc
+echo "scale=2;($fahren-32)*(5/9)"|bc| 
+echo "scale=2;($fahren-32)*(5/9)+273"|bc 
 
